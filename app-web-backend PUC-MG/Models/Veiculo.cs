6765011225c8d1e1b49bc25f0dd53494c45b550f
Ramon Vicente Silva <vicente.silva.ramon@gmail.com>
2023-02-14 18:05:28 -0300

    feat: Adicionar o model veiculo

---
+++ app-web-backend PUC-MG/Models/Veiculo.cs
@@ -0,0 +1,10 @@
+﻿namespace app_web_backend_PUC_MG.Models
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public string Placa { get; set; }
+    }
+}
